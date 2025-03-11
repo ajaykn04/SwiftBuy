0f24a4f36e0c70dd5e853b95b83ccb998f89285c
ajaykn_04 <ajaykn04@gmail.com>
2025-03-11 07:36:23 +0530

    merged fix

--- frontend/src/components/MyCart.jsx
+++ frontend/src/components/MyCart.jsx
@@ -293,6 +293,7 @@ const MyCart = () => {
           ))}
         </List>
       ) : (
+<<<<<<< HEAD
         
           <Box
             sx={{
@@ -547,6 +548,251 @@ const MyCart = () => {
             </Box>
           </Box>
         
+=======
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "flex-start",
+            marginTop: "10vh",
+            width: "90%",
+            justifyContent: "space-between",
+          }}
+        >
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell
+                    sx={{
+                      fontFamily: "fantasy",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "3vh",
+                      width: "600px",
+                    }}
+                  >
+                    ITEM
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      fontFamily: "fantasy",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "3vh",
+                    }}
+                  >
+                    PRICE
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      fontFamily: "fantasy",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "3vh",
+                    }}
+                  >
+                    QUANTITY
+                  </TableCell>
+                  <TableCell
+                    sx={{
+                      fontFamily: "fantasy",
+                      color: "white",
+                      fontWeight: "bold",
+                      fontSize: "3vh",
+                    }}
+                  >
+                    TOTAL
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {products.map((product, index) => (
+                  <TableRow key={index}>
+                    <TableCell
+                      sx={{
+                        fontFamily: "cursive",
+                        color: "white",
+                        display: "flex",
+                        alignItems: "flex-start",
+                      }}
+                    >
+                      <Box>
+                        <img
+                          src={`${api_url}/${product.product.image}`}
+                          alt={product.product.name}
+                          style={{
+                            width: 150,
+                            height: "auto",
+                            cursor: "pointer",
+                          }}
+                        />
+                      </Box>
+                      <Box sx={{ mt: 13.5, ml: 6 }}>
+                        <Typography
+                          className="productname"
+                          sx={{
+                            fontFamily: "fantasy",
+                            color: "white",
+                            fontSize: 32,
+                            mt: -10,
+                          }}
+                        >
+                          {product.product.name}
+                        </Typography>
+                        <Box sx={{ display: "flex", alignItems: "center" }}>
+                          <Box
+                            sx={{
+                              mt: 1,
+                              display: "flex",
+                              alignItems: "center",
+                              backgroundColor: "#222",
+                              color: "white",
+                              borderRadius: "8px",
+                              padding: "4px 8px",
+                              fontSize: "14px",
+                              fontWeight: "bold",
+                              width: "fit-content",
+                            }}
+                          >
+                            <Typography sx={{ mr: 0.5, color: "#FFAD18" }}>
+                              {parseFloat(product.product.rating.toFixed(1)) ||
+                                0}{" "}
+                              ⭐
+                            </Typography>
+                          </Box>
+                          <Typography
+                            sx={{
+                              color: "white",
+                              fontFamily: "cursive",
+                              ml: 1,
+                              mt: 0.8,
+                            }}
+                          >
+                            {product.product.reviews.length === 0
+                              ? "No Rating"
+                              : product.product.reviews.length === 1
+                              ? "1 Rating"
+                              : `${product.product.reviews.length} Ratings`}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        sx={{ mt: 1.5, fontFamily: "cursive", color: "yellow" }}
+                      >
+                        ₹{product.product.price}
+                      </Typography>
+                    </TableCell>
+                    <TableCell>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          border: "2px solid white",
+                          borderRadius: "8px",
+                          width: "100px",
+                          height: "25px",
+                          overflow: "hidden",
+                        }}
+                      >
+                        <Button
+                          sx={{
+                            minWidth: "30px",
+                            fontSize: "20px",
+                            borderRight: "2px solid white",
+                            borderRadius: "0",
+                            color: "white",
+                          }}
+                          onClick={async () => {
+                            try {
+                              product.quantity -= 1;
+                              await axios.post(
+                                `${api_url}/user/cart/updateitemquantity/${data._id}/${product.product._id}/${product.quantity}`
+                              );
+                              window.location.reload(true);
+                            } catch (error) {
+                              console.error(error);
+                            }
+                          }}
+                        >
+                          -
+                        </Button>
+
+                        <Typography
+                          type="text"
+                          sx={{
+                            width: "40px",
+                            textAlign: "center",
+                            fontSize: "16px",
+                            background: "transparent",
+                            color: "white",
+                          }}
+                        >
+                          {product.quantity}
+                        </Typography>
+
+                        <Button
+                          sx={{
+                            minWidth: "30px",
+                            fontSize: "20px",
+                            borderLeft: "2px solid white",
+                            borderRadius: "0",
+                            color: "white",
+                          }}
+                          onClick={async () => {
+                            try {
+                              product.quantity += 1;
+                              await axios.post(
+                                `${api_url}/user/cart/updateitemquantity/${data._id}/${product.product._id}/${product.quantity}`
+                              );
+                              window.location.reload(true);
+                            } catch (error) {
+                              console.error(error);
+                            }
+                          }}
+                        >
+                          +
+                        </Button>
+                      </Box>
+                    </TableCell>
+                    <TableCell>
+                      <Typography
+                        sx={{ mt: 1.5, fontFamily: "cursive", color: "yellow" }}
+                      >
+                        ₹{`${product.product.price * product.quantity}`}
+                      </Typography>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Box
+            sx={{
+              width: "18%",
+              mt: 1,
+              minWidth: "250px", // Keeps width consistent
+              marginLeft: "20px", // Adds spacing
+              position: "sticky", // Keeps it aligned
+              top: "10vh", // Aligns with table header
+              alignSelf: "start", // Ensures it follows the table header's height
+            }}
+          >
+            <Typography
+              sx={{
+                fontFamily: "fantasy",
+                color: "white",
+                fontWeight: "bold",
+                fontSize: "3vh",
+                width: "300px",
+              }}
+            >
+              SUMMARY
+            </Typography>
+          </Box>
+        </Box>
+>>>>>>> 38aa78a408568d406ff36d1f31a3da436d47651d
       )}
     </div>
   );
