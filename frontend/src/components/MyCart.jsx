98dd5a1ed08a0e3b6ca0ede43bb0595a129aa9de
ajaykn_04 <ajaykn04@gmail.com>
2025-03-18 22:08:00 +0530

    cart not available added

--- frontend/src/components/MyCart.jsx
+++ frontend/src/components/MyCart.jsx
@@ -28,19 +28,14 @@ const MyCart = () => {
   const [empty, setEmpty] = useState(true);
   const api_url = import.meta.env.VITE_API_URL;
   const data = JSON.parse(localStorage.getItem("userData"));
-  const [test, setTest] = useState(3);
 
   useEffect(() => {
     const apiUrl = `${api_url}/user/getcart/${data._id}`;
     axios
       .get(apiUrl)
       .then((response) => {
-        const updatedProducts = response.data.map((product) => ({
-          ...product,
-          available: "", // Adding the 'available' property
-        }));
-        setProducts(updatedProducts);
-        setEmpty(updatedProducts.length === 0);
+        setProducts(response.data);
+        setEmpty(response.data.length === 0);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
@@ -70,232 +65,6 @@ const MyCart = () => {
             Your cart is empty
           </Typography>
         </center>
-      ) : test === 1 ? (
-        <Grid container spacing={2} sx={{ mt: "70px" }}>
-          {products.map((product, index) => (
-            <Grid
-              item
-              xs={12}
-              sm={15}
-              md={3}
-              lg={2.3}
-              key={index}
-              sx={{ ml: "9px", mt: -2 }}
-            >
-              <Paper
-                elevation={3}
-                sx={{
-                  padding: 1,
-                  backgroundColor: "currentcolor",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  height: "325px",
-                }}
-              >
-                <Button
-                  variant="outlined"
-                  sx={{
-                    overflow: "hidden",
-                    borderColor: "white",
-                    borderRadius: "15px",
-                    width: "255px",
-                    height: "325px",
-                    "&:hover": {
-                      borderColor: "darkorange",
-                    },
-                  }}
-                  onClick={() => {
-                    navigate("/detproduct", { state: product.product });
-                  }}
-                  style={{
-                    fontSize: "20px",
-                    fontFamily: "fantasy",
-                    color: "black",
-                  }}
-                >
-                  <Container
-                    style={{
-                      backgroundColor: "currentcolor",
-                      flex: 1,
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "flex-start",
-                    }}
-                  >
-                    <img
-                      src={`${api_url}/${product.product.image}`}
-                      alt={product.product.name}
-                      style={{
-                        marginLeft: "-39px",
-                        marginTop: "-10px",
-                        width: "261px",
-                        height: "260px",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <Typography
-                      variant="subtitle1"
-                      fontFamily={"cursive"}
-                      sx={{
-                        ml: -1.5,
-                        mt: 1,
-                        color: "white",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      {product.product.name}
-                    </Typography>
-
-                    <Typography
-                      variant="subtitle1"
-                      fontFamily={"cursive"}
-                      sx={{
-                        ml: 18,
-                        mt: 0.5,
-                        mb: -2.9,
-                        color: "yellow",
-                        fontWeight: "bold",
-                      }}
-                    >
-                      ₹{product.product.price}
-                    </Typography>
-
-                    <Rating
-                      name={`rating-${index}`}
-                      value={product.product.rating || 0}
-                      readOnly
-                      precision={0.1}
-                      sx={{
-                        ml: -2,
-                        mb: 1,
-                        mt: -0.5,
-                        "& .MuiRating-iconFilled": {
-                          color: "#FFAD18",
-                        },
-                        "& .MuiRating-iconEmpty": {
-                          color: "grey",
-                        },
-                        "& .MuiRating-icon:hover": {
-                          borderColor: "darkorange",
-                        },
-                      }}
-                    />
-                  </Container>
-                </Button>
-              </Paper>
-            </Grid>
-          ))}
-        </Grid>
-      ) : test === 2 ? (
-        <List sx={{ marginTop: "10vh" }}>
-          {products.map((product, index) => (
-            <Box
-              key={index}
-              sx={{
-                maxWidth: 1500,
-                mx: "auto",
-              }}
-            >
-              <ListItem
-                key={index}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  justifyContent: "flex-start",
-                  padding: "16px",
-                  borderBottom: "1px solid gray",
-                }}
-              >
-                <Button
-                  key={index}
-                  component="div"
-                  sx={{
-                    all: "inherit",
-                    width: "100%",
-                    cursor: "pointer",
-                    borderRadius: "20px",
-                    border: "none",
-                    "&:hover .productname": {
-                      color: "darkorange",
-                    },
-                  }}
-                  style={{ color: "orange" }}
-                  onClick={() => {
-                    navigate("/detproduct", { state: product.product });
-                  }}
-                >
-                  <Box>
-                    <img
-                      src={`${api_url}/${product.product.image}`}
-                      alt={product.product.name}
-                      style={{
-                        width: 150,
-                        height: "auto",
-                        cursor: "pointer",
-                      }}
-                    />
-                  </Box>
-                  <Box sx={{ mt: 8, ml: 6 }}>
-                    <Typography
-                      className="productname"
-                      sx={{
-                        fontFamily: "fantasy",
-                        color: "white",
-                        fontSize: 32,
-                        mt: -10,
-                      }}
-                    >
-                      {product.product.name}
-                    </Typography>
-                    <Box sx={{ display: "flex", alignItems: "center" }}>
-                      <Box
-                        sx={{
-                          mt: 1,
-                          display: "flex",
-                          alignItems: "center",
-                          backgroundColor: "#222",
-                          color: "white",
-                          borderRadius: "8px",
-                          padding: "4px 8px",
-                          fontSize: "14px",
-                          fontWeight: "bold",
-                          width: "fit-content",
-                        }}
-                      >
-                        <Typography sx={{ mr: 0.5, color: "#FFAD18" }}>
-                          {parseFloat(product.product.rating.toFixed(1)) || 0}{" "}
-                          ⭐
-                        </Typography>
-                      </Box>
-                      <Typography
-                        sx={{
-                          color: "white",
-                          fontFamily: "cursive",
-                          ml: 1,
-                          mt: 0.8,
-                        }}
-                      >
-                        {product.product.reviews.length === 0
-                          ? "No Rating"
-                          : product.product.reviews.length === 1
-                          ? "1 Rating"
-                          : `${product.product.reviews.length} Ratings`}
-                      </Typography>
-                    </Box>
-
-                    <Typography
-                      sx={{ mt: 1.5, fontFamily: "cursive", color: "yellow" }}
-                    >
-                      ₹{product.product.price}
-                    </Typography>
-                  </Box>
-                </Button>
-              </ListItem>
-            </Box>
-          ))}
-        </List>
       ) : (
         <Box
           sx={{
@@ -468,11 +237,17 @@ const MyCart = () => {
                               }
                             } else {
                               try {
+                                setProducts((prevProducts) =>
+                                  prevProducts.map((p) =>
+                                    p.product._id === product.product._id
+                                      ? { ...p, quantity: p.quantity - 1 }
+                                      : p
+                                  )
+                                );
                                 product.quantity -= 1;
                                 await axios.post(
                                   `${api_url}/user/cart/updateitemquantity/${data._id}/${product.product._id}/${product.quantity}`
                                 );
-                                window.location.reload(true);
                               } catch (error) {
                                 console.error(error);
                               }
@@ -504,27 +279,41 @@ const MyCart = () => {
                             color: "white",
                           }}
                           onClick={async () => {
-                            if (product.quantity >= product.product.stock) {
-                              product.available="Not available"
-                              console.log(product.available)
-                            } else {
+                            setProducts((prevProducts) =>
+                              prevProducts.map((p) =>
+                                p.product._id === product.product._id
+                                  ? p.quantity >= p.product.stock
+                                    ? { ...p, available: "Not available" }
+                                    : {
+                                        ...p,
+                                        quantity: p.quantity + 1,
+                                        available: "",
+                                      }
+                                  : p
+                              )
+                            );
+
+                            if (product.quantity < product.product.stock) {
                               try {
-                                product.quantity += 1;
                                 await axios.post(
-                                  `${api_url}/user/cart/updateitemquantity/${data._id}/${product.product._id}/${product.quantity}`
+                                  `${api_url}/user/cart/updateitemquantity/${
+                                    data._id
+                                  }/${product.product._id}/${
+                                    product.quantity + 1
+                                  }`
                                 );
-                                window.location.reload(true);
                               } catch (error) {
                                 console.error(error);
                               }
-                              console.log(product.available)
                             }
                           }}
                         >
                           +
                         </Button>
                       </Box>
-                      <Typography sx={{ color: "red" }}>{product.available}</Typography>
+                      <Typography sx={{ color: "red",minHeight: "25px", mb:-4.5,mt:.5,ml:.5 }}>
+                        {product.available}
+                      </Typography>
                     </TableCell>
                     <TableCell>
                       <Typography
